a27065c44f284bbcc2e99731c6ba048ed60d642a
Aleksander Aniśko <anisko.aleksander@yahoo.com>
2022-05-18 20:24:12 +0200

    DisplayResult

--- js/script.js
+++ js/script.js
@@ -27,3 +27,19 @@ if (
 } else {
   printMessage("wygrał komputer");
 }
+
+function displayResult(argComputerMove, argPlayerMove) {
+  if (
+    (argComputerMove == "kamień" && argPlayerMove == "papier") ||
+    (argComputerMove == "papier" && argPlayerMove == "nożyce") ||
+    (cargComputerMove == "nożyce" && argPlayerMove == "kamień")
+  ) {
+    printMessage("Ty wygrywasz!");
+  } else if (argComputerMove == argPlayerMove) {
+    printMessage("remis");
+  } else if (argPlayerMove == "nieznany ruch") {
+    printMessage("nieprawidołowy ruch");
+  } else {
+    printMessage("wygrał komputer");
+  }
+}
