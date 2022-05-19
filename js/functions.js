5d17844b16b80997d5a32cbf8e8270cc64e7bf97
Aleksander Aniśko <anisko.aleksander@yahoo.com>
2022-05-19 18:24:24 +0200

    Changed let to const and var

--- js/functions.js
+++ js/functions.js
@@ -38,17 +38,17 @@ function displayResult(argComputerMove, argPlayerMove) {
 
 function playGame(playerInput) {
   clearMessages();
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
 
   console.log("Wylosowana liczba to: " + randomNumber);
 
-  let computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
 
   printMessage("Mój ruch to: " + computerMove);
 
   console.log("Gracz wpisał: " + playerInput);
 
-  let playerMove = getMoveName(playerInput);
+  var playerMove = getMoveName(playerInput);
 
   printMessage("Twój ruch to: " + playerMove);
 
